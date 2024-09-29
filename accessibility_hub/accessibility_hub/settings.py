9ba11683540be91da6afe42fc8bf3ad107e21eca
hamza-1085504 <1085504@hr.nl>
2024-09-29 21:56:53 +0200

    hardcoded secret fix

--- accessibility_hub/accessibility_hub/settings.py
+++ accessibility_hub/accessibility_hub/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 from django.utils.translation import gettext_lazy as _
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4=8ni*m!u^1dhffuzqn!!j$())ebeob-+@waoj*g)$+&u-s=*p'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
